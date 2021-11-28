377f23d293ebde01b028ecb761df245e532172b2
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2021-11-28 16:40:01 +0100

    Make timer safely restartable

--- timercpp.h
+++ timercpp.h
@@ -3,39 +3,79 @@
 #include <chrono>
 #include <atomic>
 #include <functional>
+#include <memory>
+#include <thread>
 
 class Timer {
-	std::atomic<bool> active{true};
-    public:
-        void setTimeout(std::function<void()> function, int delay);
-        void setInterval(std::function<void()> function, int interval);
-        void stop();
+public:
+	Timer() {}
+	virtual ~Timer() {
+		stop();
+	}
+	void setTimeout(std::function<void()> function, int delay);
+	void setInterval(std::function<void()> function, int interval);
+	bool isActive();
+	void stop();
+private:
+	std::atomic_bool active{true};
+	std::unique_ptr<std::thread> thread;
 
 };
 
 void Timer::setTimeout(std::function<void()> function, int delay) {
-    active = true;
-    std::thread t([=]() {
+	if (active.load()) {
+		stop();
+	}
+    active.store(true);
+    thread = std::make_unique<std::thread>([=]() {
         if(!active.load()) return;
         std::this_thread::sleep_for(std::chrono::milliseconds(delay));
         if(!active.load()) return;
         function();
     });
-    t.detach();
 }
 
 void Timer::setInterval(std::function<void()> function, int interval) {
-    active = true;
-    std::thread t([=]() {
+	if (active.load()) {
+		stop();
+	}
+    active.store(true);
+	thread = std::make_unique<std::thread>([=]() {
         while(active.load()) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+			if (interval > 1'000) {
+				auto start = std::chrono::steady_clock::now();
+				while(active.load()) {
+					std::this_thread::sleep_for(
+							std::chrono::milliseconds(1'000));
+					auto current = std::chrono::steady_clock::now();
+					auto durationMs =
+						std::chrono::duration_cast<std::chrono::milliseconds>
+							(current - start).count();
+					auto diff = interval-durationMs;
+					if (diff <= 0) {
+						break;
+					} else if (diff <= 1'000){
+						std::this_thread::sleep_for(
+								std::chrono::milliseconds(diff));
+					}
+				}
+			} else {
+				std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+			}
             if(!active.load()) return;
             function();
         }
     });
-    t.detach();
+}
+
+bool Timer::isActive() {
+	return active.load();
 }
 
 void Timer::stop() {
-    active = false;
+    active.store(false);
+	if (thread) {
+		thread->join();
+		thread.reset();
+	}
 }
